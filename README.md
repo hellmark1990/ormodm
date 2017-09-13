382fdf3aed08a07a1dcb108b89fe57871972683e
Marko Shemechko <mshemechko@n-ix.com>
2017-09-13 15:12:25 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+test.loc
+========
+
+A Symfony project created on September 13, 2017, 1:29 pm.
---
+++ src/Mshem/OrmOdmRelationBundle/OrmOdmRelationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mshem\OrmOdmRelationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OrmOdmRelationBundle extends Bundle
+{
+}
